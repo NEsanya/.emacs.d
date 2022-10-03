9adc0ad9f8bf0492de91b277c42b96295b2a5b20
NEsanya <nesanya1990@gmail.com>
2022-10-04 02:41:16 +0300

    LSP config for Rust and C/C++.

--- configurate-packages/eglot.el
+++ configurate-packages/eglot.el
@@ -1,1 +1,9 @@
-(setq eglot-rust-server 'rust-analyzer)
+(require 'use-package)
+
+(use-package eglot
+  :init
+  :config
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+  (add-to-list 'eglot-server-programs '((rust-mode)  "rust-analyzer"))
+  (add-hook 'rust-mode-hook 'eglot-ensure)
+  (add-hook 'c-mode-hook 'eglot-ensure))
